f06660f68fb329d903307cc178537650c1d0df45
jpena <juan.pena@wmcontractor.com>
2025-01-18 17:31:20 -0500

    Docker script congfiguration

--- prisma/migrations/20250117174829_init/migration.sql
+++ prisma/migrations/20250117174829_init/migration.sql
@@ -2,7 +2,12 @@
 CREATE TABLE "Product" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
+    "description" TEXT,
     "price" REAL NOT NULL,
+    "available" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
+-- CreateIndex
+CREATE INDEX "Product_available_idx" ON "Product"("available");
